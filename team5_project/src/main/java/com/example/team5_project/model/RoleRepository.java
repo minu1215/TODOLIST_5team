766440642c3dbbb5f821e2e5f355035990a3ed4c
김민우 <mkw3295@gmail.com>
2024-01-09 17:19:54 +0900

    240109_Long_to_Integer

--- team5_project/src/main/java/com/example/team5_project/model/RoleRepository.java
+++ team5_project/src/main/java/com/example/team5_project/model/RoleRepository.java
@@ -2,7 +2,7 @@ package com.example.team5_project.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 	Role findByName(ERole roleUser);
 
